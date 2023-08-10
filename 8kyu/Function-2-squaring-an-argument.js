75c0f2de1d81b8b9aa26f3169e9793f22d2516cc
Joseph Keepers <josephkeepers@gmail.com>
2023-08-10 09:56:07 -0400

    added new codwars challenge

---
+++ 8kyu/Function-2-squaring-an-argument.js
@@ -0,0 +1,16 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+//My solution
+
+// Write the "square"-function here
+function square(arg){
+    //find square of arg
+    // return it
+    return Math.pow(arg, 2)
+  }
+  
+  //PREP
+  //P: argument, num, + or -
+  //R: return square of the argument
+  //E:squareRoot(11) -> 121, squareRoot(10)->100, squareRoot(11.8321)->140
+  //P:
