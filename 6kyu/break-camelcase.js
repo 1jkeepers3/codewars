b70d353bde504a0312795ac0fd72d70a5095b6f3
Joseph Keepers <josephkeepers@gmail.com>
2023-09-26 09:52:27 -0400

    added new codewars challenge

---
+++ 6kyu/break-camelcase.js
@@ -0,0 +1,18 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+//My solution
+
+function solution(string) {
+    string = string.split('').map(function (el) {
+      if (el === el.toUpperCase()) {
+        el = ' ' + el
+      }
+      return el
+    })
+      return string.join('')
+  }
